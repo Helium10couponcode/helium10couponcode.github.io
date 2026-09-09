54051d3ad3c1c24435788add4c3f8df997e35fdc
Helium 10 <thegameranimecom@gmail.com>
2026-09-09 12:42:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const AFFILIATE_URL = "https://allcouponcodes.net/refer/helium10";
 
 // Coupon Data
 const coupons = [
-  { code: " 26AUG30OFFP", discount: "30% OFF", description: "Get 30% Off for 6 Months Limited Time Offer", type: "exclusive", initialDays: 3 },
   { code: "AFF_EXITPOP25", discount: "25% OFF", description: "Get 25% Off for 6 Months Limited Time Offer", type: "exclusive", initialDays: 3 },
   { code: "ALLCOUPONCODES20", discount: "20% OFF", description: "20% off for 6 months on Platinum, Diamond & Elite plans", type: "exclusive", initialDays: 3 },
   { code: "ALLCOUPONCODES10", discount: "10% OFF", description: "10% off forever on all monthly and annual subscriptions", type: "normal", initialDays: 5 },
